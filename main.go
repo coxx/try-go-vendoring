9186fdb8f7423549b7b821d48eeec0a05f418e02
Konstantin Cherkasov <konstantin.cherkasov@lazada.com>
2016-01-29 01:56:32 +0300

    Uncomment line which will not compile because of mismatched types

--- main.go
+++ main.go
@@ -12,5 +12,5 @@ func main() {
 	fmt.Printf("main.main(): xxx.B == %#v\n", yyy.B)
 	fmt.Printf("main.main(): yyy.A == %#v\n", yyy.A)
 
-	//fmt.Printf("%#v\n", xxx.A == yyy.A)
+	fmt.Printf("%#v\n", xxx.A == yyy.A) // this line will not compile
 }
